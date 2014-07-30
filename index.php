8095eb423a97545676e02cca5baeb85ff18334ac
katriniya <katriniya.a@gmail.com>
2014-07-30 18:57:59 +0400

    Adding index.html to the repository

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+defined('_JEXEC') or die;
+JHtml::_('behavior.framework', true);
+$app = JFactory::getApplication();
+?>
+<?php echo '<?'; ?>xml version="1.0" encoding="<?php echo $this->_charset ?>"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
